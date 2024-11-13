fa235d27366215579e9643b0639d7e7ce3e28bf1
unknown <rmjesusaddict@gmail.com>
2024-11-13 16:09:49 +0300

    Add the popup window design

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 function mobileMenu() {
     const hamburger = document.querySelector('.menu-bar');
-    const mobileMenu = document.getElementById('menu-page')
-    const topMenu = document.querySelector('.top-menu');
+    const mobileMenu = document.getElementById('menu-page');
     const xButton = document.querySelector('.top-x');
 
     if (mobileMenu.style.display === 'block') {
@@ -14,3 +13,34 @@ function mobileMenu() {
         xButton.style.display = 'block';
     }
 }
+
+function showPopup() {
+    const mainPopupDiv = document.createElement('div');
+    mainPopupDiv.classList.add('popup');
+    const secondaryPopupDiv = document.createElement('div')
+    secondaryPopupDiv.classList.add('popup_content');
+    mainPopupDiv.appendChild(secondaryPopupDiv);
+    const imageDiv = document.createElement('div');
+    imageDiv.setAttribute('id','popup_img');
+    const img = document.createElement('img');
+    img.setAttribute('src', '../assets/images/Snapshot Portfolio.jpg');
+    img.setAttribute('width', '311px');
+    img.setAttribute('height', '220px');
+    imageDiv.appendChild(img);
+    const titleSection = document.createElement('div');
+    titleSection.classList.add('title-popup');
+    const mainProjectTitle = document.createElement('h2');
+    mainProjectTitle.innerText = "Keeping track of hundreds of components"
+    titleSection.appendChild(mainProjectTitle);
+    secondaryPopupDiv.appendChild(imageDiv);
+    secondaryPopupDiv.appendChild(titleSection);
+    
+    return mainPopupDiv;
+}
+
+window.addEventListener('load', ()=> {
+   const popupWindowSection = document.getElementById('popWindow');
+   if (popupWindowSection) {
+        popupWindowSection.appendChild(showPopup());
+   }
+})
